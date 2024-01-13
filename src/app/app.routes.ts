ccb595edee3e2533263a50dcd7c2847cffd88a5c
Hermansyah Efendi <href.dev@gmail.com>
2024-01-13 22:59:38 +0700

    update\

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,22 @@ import { Routes } from '@angular/router';
 import { ProductComponent } from './features/product/product.component';
 import { MainComponent } from './shared/layout/main/main.component';
 import { TestComponent } from './features/test/test.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
-{
-  path : '', component : MainComponent, 
-  children : [ 
-    { path : 'product', component : ProductComponent } ,
-    { path : 'test', component : TestComponent } 
-  ]
-}
+  {
+    path : 'login', component : LoginComponent
+  },
+  {
+    path : 'register', component : RegisterComponent
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: 'product', component: ProductComponent },
+      { path: 'test', component: TestComponent },
+    ],
+  },
 ];
